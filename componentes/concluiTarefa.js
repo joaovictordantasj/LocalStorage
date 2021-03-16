68afe53542b7ca4543ac63596db80985c6c4c3ce
João Victor <joaovictordjardim@gmail.com>
2021-03-16 09:47:45 -0300

    Funcionalidade de concluir tarefas feita

--- componentes/concluiTarefa.js
+++ componentes/concluiTarefa.js
@@ -1,22 +1,20 @@
-const BotaoConclui = () => { 
-    const botaoConclui = document.createElement('button');
-    
-    botaoConclui.classList.add('check-button');
-    botaoConclui.innerText = 'concluir';
+const concluirTarefa = (atualiza, id) => {
+  const tarefasCadastradas = JSON.parse(localStorage.getItem("Tarefas"));
+  tarefasCadastradas[id].concluida = !tarefasCadastradas[id].concluida;
+  localStorage.setItem("Tarefas", JSON.stringify(tarefasCadastradas));
 
-    botaoConclui.addEventListener('click', concluirTarefa);
+  atualiza();
+};
 
-    return botaoConclui;
+const BotaoConclui = (atualiza, id) => {
+  const botaoConclui = document.createElement("button");
 
-}
+  botaoConclui.classList.add("check-button");
+  botaoConclui.innerText = "concluir";
 
-const concluirTarefa = (evento) => {
-    const botaoConclui = evento.target;
+  botaoConclui.addEventListener("click", () => concluirTarefa(atualiza, id));
 
-    const tarefaCompleta = botaoConclui.parentElement;
-
-    tarefaCompleta.classList.toggle('done');
-}
+  return botaoConclui;
+};
 
 export default BotaoConclui;
-
--- componentes/criaTarefa.js
+++ componentes/criaTarefa.js
@@ -1,43 +1,49 @@
-import { carregaTarefa } from './carregaTarefa.js';
-import BotaoConclui from './concluiTarefa.js';
-import BotaoDeleta from './deletaTarefa.js';
- 
+import { carregaTarefa } from "./carregaTarefa.js";
+import BotaoConclui from "./concluiTarefa.js";
+import BotaoDeleta from "./deletaTarefa.js";
+
 export const handleNovoItem = (e) => {
   e.preventDefault();
 
-  const tarefas = JSON.parse(localStorage.getItem('Tarefas')) || [];
-  const input = document.querySelector('[data-form-input]');
+  const tarefas = JSON.parse(localStorage.getItem("Tarefas")) || [];
+  const input = document.querySelector("[data-form-input]");
   const valor = input.value;
-  
-  const calendario = document.querySelector('[data-form-date]');
+
+  const calendario = document.querySelector("[data-form-date]");
   const data = moment(calendario.value);
-  const dataFormatada = data.format('DD/MM/YYYY, h:mm a');
+  const dataFormatada = data.format("DD/MM/YYYY, h:mm a");
+  const concluida = false;
 
   const dados = {
     valor,
-    dataFormatada
-  }
+    dataFormatada,
+    concluida,
+  };
 
   const tarefasAtualizadas = [...tarefas, dados];
 
-  localStorage.setItem('Tarefas', JSON.stringify(tarefasAtualizadas));
+  localStorage.setItem("Tarefas", JSON.stringify(tarefasAtualizadas));
 
-  calendario.value = '';
-  input.value = '';
+  calendario.value = "";
+  input.value = "";
 
   carregaTarefa();
-}
+};
 
-export const Tarefa = ({ valor, dataFormatada }) => {
-    
-  const tarefa = document.createElement('li');
-  tarefa.classList.add('task');
+export const Tarefa = ({ valor, dataFormatada, concluida }, id) => {
+  const tarefa = document.createElement("li");
   const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`;
-  
+
+  if (concluida) {
+    tarefa.classList.add("done");
+  }
+
+  tarefa.classList.add("task");
+
   tarefa.innerHTML = conteudo;
-  
-  tarefa.appendChild(BotaoConclui());
+
+  tarefa.appendChild(BotaoConclui(carregaTarefa, id));
   tarefa.appendChild(BotaoDeleta());
 
   return tarefa;
-}
+};
